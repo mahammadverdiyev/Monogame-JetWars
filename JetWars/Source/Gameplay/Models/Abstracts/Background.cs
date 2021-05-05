d780cd233bdb5330deba4913b6b58e02fa0b5333
Mahammad <devmahammad@gmail.com>
2021-05-05 19:57:05 +0400

    cleaner file structure

--- JetWars/Source/Gameplay/Models/Abstracts/Background.cs
+++ JetWars/Source/Gameplay/Models/Abstracts/Background.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+
 namespace JetWars.Source
 {
     public abstract class Background : Basic2D
